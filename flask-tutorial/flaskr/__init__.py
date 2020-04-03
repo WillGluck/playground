f5edd0e5b7ef8d939ddc173b9cdbb4b451b93766
willgluck <bigwillgluck@gmail.com>
2020-04-02 22:03:40 -0300

    feat: Criado blueprint de autenticação.
    Blueprint de autenticação criada com views de login, cadastro e logout;
    Decorator criado para uso futuro em views que precisam de um usuário
    autenticado;
    Método para carregamento do usuário logado criado. Configurado para
    executar sempre antes de qualquer view.
    Blueprin registrada no app.

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -21,8 +21,13 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    #Not the best practice, according to PEP8
+
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     @app.route('/hello')
     def hello():
