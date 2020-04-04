62076cb8381322d8f0727328b7f19f5b9679275f
willgluck <bigwillgluck@gmail.com>
2020-04-03 21:01:33 -0300

    fix: Corrigidos erros de sintaxe no arquivo auth.py

--- flask-tutorial/flaskr/auth.py
+++ flask-tutorial/flaskr/auth.py
@@ -23,7 +23,7 @@ def register():
         elif not password:
             error = 'Password is required.'
         elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username)
+            'SELECT id FROM user WHERE username = ?', (username,)
         ).fetchone() is not None:
             error = 'User {} is already registered.'.format(username)
         
@@ -36,22 +36,23 @@ def register():
             return redirect(url_for('auth.login'))
         
         flash(error)
+
     return render_template('auth/register.html')
 
-@bp.route('/login', method=['GET', 'POST'])
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method = 'POST':
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         db = get_db()
         error = None
         user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username)
+            'SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
 
         if user is None:
             error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password)
+        elif not check_password_hash(user['password'], password):
             error = 'Incorrect password.'
 
         if error is None:
