2a86e45d1a2e9c70a4f06ae79e3c4d4b338a5be2
willgluck <bigwillgluck@gmail.com>
2020-04-22 20:10:44 -0300

    feat: View de entradas, login e logout. Template do índice
    Adicionado as views de login, logout e para retornar todas as entradas
    Adicionado arquivo de template do índice (onde exibe todas as entradas)

--- flaskr-tdd/app.py
+++ flaskr-tdd/app.py
@@ -36,6 +36,40 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
+@app.route('/')
+def show_entries():
+    """Searches the database for entries, then displays them."""
+    db = get_db()
+    entries = db.execute(
+        'SELECT * FROM entries ORDER BY id DESC'
+    ).fetchall()
+    return render_template('index.html', entries=entries)
+
+@app.route('login', methods=['GET', 'POST'])
+def login():
+    """User login/authentication/session management."""
+    error = None
+
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    
+    return render_template('login.html', error=error)
+
+@app.route('logout')
+def logout():
+    """User logout/authentication/session management."""
+    session.pop('logged_in', None)
+    flash('You are logged out')
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     init_db()
     app.run()
+    
